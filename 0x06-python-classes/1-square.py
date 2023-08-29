3c3031538d30a56c4b13d5520ca05323ed130570
Bellomubaraq875 <bellomubaraq875@gmail.com>
2023-08-29 14:48:48 +0000

    This class defines a Square based opn size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This class defines a square"""
+
+
+class Square:
+    """This class represe3nts a square
+    This square should uahve a privatre instance
+    """
+    def __init__(self, size):
+        """The attriburte here will be made private
+        It has no type or value certification
+        """
+        self.__size = size
