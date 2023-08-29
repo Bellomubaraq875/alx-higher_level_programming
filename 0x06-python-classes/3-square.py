b98f6734bbd67ab2466e751f4a0512ae5752f8ff
Bellomubaraq875 <bellomubaraq875@gmail.com>
2023-08-29 14:52:23 +0000

    This class gets the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" this class defines a square with optiuonal size"""
+
+
+class Square:
+    """This is the classs
+    the name is Square
+    """
+    def __init__(self, size=0):
+        """size must be optional, that is why it is equal to 0
+        it must also be an integer
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """This function returns the area of a square
+        by raising the size to the power of two
+        or multiplying it by itself
+        """
+        return self.__size * self.__size
