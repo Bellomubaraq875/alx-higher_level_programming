418e566cee557e3baddadd52993457ddcc2fd53a
Bellomubaraq875 <bellomubaraq875@gmail.com>
2023-09-04 15:15:32 -0700

    removed self call

---
+++ 0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This prograsm creates an empty class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    This class is an empty rectangle
+    it is a background for future codes
+    """
+    pass
