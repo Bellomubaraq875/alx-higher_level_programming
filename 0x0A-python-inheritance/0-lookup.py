748170000ce3fbca1232287e38c268429d9bc812
Bellomubaraq875 <bellomubaraq875@gmail.com>
2023-09-11 14:42:56 -0700

    Create 0-lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This dorectory returns a list of attribytes and objects in a file
+"""
+
+
+def lookup(obj):
+    """the code that returns the
+    list of available objects and
+    methods of an object
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This directory contains ALX and Hilberton organized tasks on Inheritance in Pythin classes
