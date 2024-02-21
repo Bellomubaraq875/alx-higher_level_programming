2ff03009465fab985d2731c6f9e024b9c07049d1
Bellomubaraq875 <bellomubaraq875@gmail.com>
2024-02-21 03:08:42 +0100

    coorecting some errors

--- 0x14-javascript-web_scraping-main/0-readme.js
+++ 0x14-javascript-web_scraping-main/0-readme.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-process.argv[0] = 'node';
-function readFile (filePath) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-  });
-}
-
-if (process.argv.length === 3) {
-  const filePath = process.argv[2];
-  readFile(filePath);
-}
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
---
+++ 0x14-javascript-web_scraping-main/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// This script displays the status code of a GET request
+
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
